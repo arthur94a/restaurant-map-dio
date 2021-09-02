390d6f1385b9c2bea31f1fd4f07cdd7a45b22360
arthur94a <arthuraraujo94@hotmail.com>
2021-09-02 20:00:53 -0300

    finish project

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactStars from 'react-rating-stars-component';
 
-import restaurant from '../../assets/restaurante-fake.png';
+import restaurante from '../../assets/restaurante-fake.png';
 import { Restaurant, RestaurantInfo, Title, Address, RestaurantPhoto } from './styles';
+import Skeleton from '../Skeleton'
 
-const RestaurantCard = () => (
-    <Restaurant>
-        <RestaurantInfo>
-            <Title>Nome do Restaurante</Title>
-            <ReactStars count={5} isHalf value={4} edit={false} activeColor="#E7711C" />
-            <Address>Rua das pedrinhas, SC</Address>
-        </RestaurantInfo>
-        <RestaurantPhoto src={restaurant} alt="Restaurant" />
-    </Restaurant>
-);
+const RestaurantCard = ({ restaurant, onClick }) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    return (
+        <Restaurant onClick={onClick}>
+            <RestaurantInfo>
+                <Title>{restaurant.name}</Title>
+                <ReactStars
+                    count={5}
+                    isHalf
+                    value={restaurant.rating}
+                    edit={false}
+                    activeColor="#E7711C"
+                />
+                <Address>{restaurant.vicinity || restaurant.formatted_address}</Address>
+            </RestaurantInfo>
+            <RestaurantPhoto
+                imageLoaded={imageLoaded}
+                src={restaurant.photos ? restaurant.photos[0].getUrl() : restaurante}
+                onLoad={() => setImageLoaded(true)}
+                alt="Restaurant"
+            />
+            {!imageLoaded && <Skeleton width="100px" height="100px" />}
+        </Restaurant>
+
+    );
+};
 
 
 export default RestaurantCard;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,40 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
 
 import restaurante from '../../assets/restaurante-fake.png';
 import logo from '../../assets/logo.svg';
-import { Card, RestaurantCard, Modal } from '../../components';
+import { Card, RestaurantCard, Modal, Map, Loader, Skeleton } from '../../components';
 
-import { Container, Search, Logo, Wrapper, Map, Carousel, CarouselTitle } from './styles';
+import { Container, Search, Logo, Wrapper, Carousel, CarouselTitle, ModalTitle, ModalContent } from './styles';
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('');
-    const [modalOpened, setModalOpened] = useState(true);
+    const [query, setQuery] = useState(null);
+    const [placeId, setPlaceId] = useState(null);
+    const [modalOpened, setModalOpened] = useState(false);
+    const { restaurants, restaurantSelected } = useSelector((state) => state.restaurants);
 
     const settings = {
         dots: false,
         infinite: true,
+        autoplay: true,
         speed: 300,
         slidesToShow: 4,
         slidesToScroll: 4,
         adaptiveHeight: true,
+    }
+
+    function handleKeyPress(e) {
+        if (e.key === 'Enter') {
+            setQuery(inputValue);
+        }
+    }
+
+    function handleOpenModal(placeId) {
+        setPlaceId(placeId);
+        setModalOpened(true);
     }
 
     return (
@@ -31,24 +47,47 @@ const Home = () => {
                         outlined
                         trailingIcon={<MaterialIcon role="button" icon="search" />}
                     >
-                        <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+                        <Input value={inputValue} onKeyPress={handleKeyPress} onChange={(e) => setInputValue(e.target.value)} />
                     </TextField>
-                    <CarouselTitle>Near to you</CarouselTitle>
-                    <Carousel {...settings}>
-                        <Card photo={restaurante} title="name..." />
-                        <Card photo={restaurante} title="name..." />
-                        <Card photo={restaurante} title="name..." />
-                        <Card photo={restaurante} title="name..." />
-                        <Card photo={restaurante} title="name..." />
-                        <Card photo={restaurante} title="name..." />
-                        <Card photo={restaurante} title="name..." />
-                        <Card photo={restaurante} title="name..." />
-                    </Carousel>
+                    {restaurants.length > 0 ? (
+                        <>
+                            <CarouselTitle>Near to you</CarouselTitle>
+                            <Carousel {...settings}>
+                                {restaurants.map((restaurant) => (
+                                    <Card
+                                        key={restaurant.place_id}
+                                        photo={restaurant.photos ? restaurant.photos[0].getUrl() : restaurante}
+                                        title={restaurant.name}
+                                    />
+                                ))}
+                            </Carousel>
+                        </>
+                    ) : <Loader />}
                 </Search>
-                <RestaurantCard />
+                {restaurants.map((restaurant) => (
+                    <RestaurantCard
+                        restaurant={restaurant}
+                        onClick={() => handleOpenModal(restaurant.place_id)} />
+                ))}
             </Container>
-            <Map />
-            <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)} />
+            <Map query={query} placeId={placeId} />
+            <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)}>
+                {restaurantSelected ? (
+                    <>
+                        <ModalTitle>{restaurantSelected?.name}</ModalTitle>
+                        <ModalContent>{restaurantSelected?.formatted_phone_number}</ModalContent>
+                        <ModalContent>{restaurantSelected?.formatted_address}</ModalContent>
+                        <ModalContent>{restaurantSelected?.opening_hours?.open_now ? 'Aberto Agora' : 'Fechado Neste Momento'}</ModalContent>
+                    </>
+                ) : (
+                    <>
+                        <Skeleton width="10px" height="10px" />
+                        <Skeleton width="10px" height="10px" />
+                        <Skeleton width="10px" height="10px" />
+                        <Skeleton width="10px" height="10px" />
+                    </>
+                )}
+            </Modal>
         </Wrapper>
     );
 };
